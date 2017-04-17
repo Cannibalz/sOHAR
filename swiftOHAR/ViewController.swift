af6f8d5398a11cd1224f90b0846bf475eea9e6a0
Tom Cruise <TomCruise@184pc53.csie.ntnu.edu.tw>
2017-04-17 13:13:23 +0800

    Initial Commit

---
+++ swiftOHAR/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  swiftOHAR
+//
+//  Created by Tom Cruise on 2017/4/17.
+//  Copyright © 2017年 Tom Cruise. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
