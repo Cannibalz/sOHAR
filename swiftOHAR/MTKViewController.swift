a2a3f3695aaad44dc9149f36a2e09eaeeaa6956f
Tom Cruise <TomCruise@184pc53.csie.ntnu.edu.tw>
2017-06-14 13:59:04 +0800

    delete

--- swiftOHAR/MTKViewController.swift
+++ swiftOHAR/MTKViewController.swift
@@ -15,28 +15,29 @@ struct Constants {
     var normalMatrix = matrix_identity_float3x3
 }
 
-class mtkVC : NSObject//, MTKViewDelegate
-{
-    weak var view: MTKView!
+//class mtkVC : NSObject//, MTKViewDelegate
+//{
+//    weak var view: MTKView!
+//
+//    let device: MTLDevice
+//    let commandQueue: MTLCommandQueue
+//    let renderPipelineState: MTLRenderPipelineState
+//    let depthStencilState: MTLDepthStencilState
+//    let sampler: MTLSamplerState
+//    let texture: MTLTexture
+//    //let mesh: Mesh
+//    var time = TimeInterval(0.0)
+//    var constants = Constants()
+//    
+//    init?(mtkVIew: MTKView)
+//    {
+//        view = mtkVIew
+//        view.sampleCount = 4
+//        view.clearColor = MTLClearColorMake(1,1,1,1)
+//        view.colorPixelFormat = .bgra8Unorm
+//        view.depthStencilPixelFormat = .depth32Float
+//        view.framebufferOnly = true
+//        view.frame = (view.layer?.frame)!
+//    }
+//}
 
-    let device: MTLDevice
-    let commandQueue: MTLCommandQueue
-    let renderPipelineState: MTLRenderPipelineState
-    let depthStencilState: MTLDepthStencilState
-    let sampler: MTLSamplerState
-    let texture: MTLTexture
-    //let mesh: Mesh
-    var time = TimeInterval(0.0)
-    var constants = Constants()
-    
-    init?(mtkVIew: MTKView)
-    {
-        view = mtkVIew
-        view.sampleCount = 4
-        view.clearColor = MTLClearColorMake(1,1,1,1)
-        view.colorPixelFormat = .bgra8Unorm
-        view.depthStencilPixelFormat = .depth32Float
-        view.framebufferOnly = true
-        view.frame = (view.layer?.frame)!
-    }
-}
