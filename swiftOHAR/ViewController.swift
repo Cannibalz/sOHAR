21c5bba634327b53e07b51ec6fa06daf86f932ed
Tom Cruise <TomCruise@184pc53.csie.ntnu.edu.tw>
2017-06-19 14:45:44 +0800

    abandoned

--- swiftOHAR/ViewController.swift
+++ swiftOHAR/ViewController.swift
@@ -39,12 +39,6 @@ class ViewController: NSViewController {
         rs.waitForNextFrame()
         colorView.image = rs.nsColorImage()
     }
-    
-
-    func delay(_ delay:Double, closure:@escaping ()->()) {
-        DispatchQueue.main.asyncAfter(
-            deadline: DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: closure)
-    }
 }
 
 
